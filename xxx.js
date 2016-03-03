60cb558ce8985ff036b6c209f98e2cb28ffc03a8
rezki-olx <rezki.bangsawan@olx.co.id>
2016-03-04 03:37:35 +0700

    SUCCESS. template engine & favicon

--- xxx.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import fs from 'fs';
-import path from 'path';
-
-const templateFile = path.join( __dirname, 'docs', '_templates', 'documentation.js' );
-console.log( templateFile );
